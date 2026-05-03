6670ce0d6061bf3fd40b9c4bc81de0757ab4cf39
farhad202526 <fa99.official@gmail.com>
2026-05-03 12:03:52 +0600

    fix avater imge show

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,4 @@
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -7,20 +8,63 @@ const db = client.db("qurbanihat");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-    emailAndPassword: { 
+  // বিল্ড এরর ফিক্স করার জন্য নিচের ২ লাইন অত্যন্ত জরুরি
+  baseURL: process.env.BETTER_AUTH_URL || "https://qurbanihat-livestock-booking-by-far-pi.vercel.app",
+  secret: process.env.BETTER_AUTH_SECRET, 
+
+  emailAndPassword: { 
     enabled: true, 
   },
   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-            // clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }, 
-    }
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }, 
+  }
 });
+
+
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient(process.env.MONGODB_URI);
+// const db = client.db("qurbanihat");
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+//     // Optional: if you don't provide a client, database transactions won't be enabled.
+//     client
+//   }),
+//     emailAndPassword: { 
+//     enabled: true, 
+//   },
+//   socialProviders: {
+//         google: { 
+//             clientId: process.env.GOOGLE_CLIENT_ID, 
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//             // clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//         }, 
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
