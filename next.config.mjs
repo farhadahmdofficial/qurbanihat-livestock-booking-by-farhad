68118ef699e122637b7016e0af9e217640e73788
farhad202526 <fa99.official@gmail.com>
2026-05-03 11:55:01 +0600

    add auth url

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**', // এখানে '/**' হবে যাতে এই ডোমেইনের সব ইমেজ সাপোর্ট করে
-      },
+      // {
+      //   protocol: 'https',
+      //   hostname: 'lh3.googleusercontent.com',
+      //   port: '',
+      //   pathname: '/**', 
+      // },
      
      {
         protocol: 'https',
